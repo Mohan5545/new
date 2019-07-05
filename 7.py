ef4f9717a4cef125b0bcb7def2d8ffdb7c84d835
Mohan <48092723+Mohan5545@users.noreply.github.com>
2019-07-05 09:42:35 +0530

    Update 7.py

--- 7.py
+++ 7.py
@@ -1,9 +1,9 @@
-n=str(input())
-list1=list(n)
-list2=[]
-for i in range(0,len(list1)):
+x=str(input())
+listm=list(x)
+listn=[]
+for i in range(0,len(listm)):
     if(i%2==0):
-        list2.insert(i+1,list1[i])
+        listn.insert(i+1,listm[i])
     elif(i%2==1):
-        list2.insert(i-1,list1[i])
-print(''.join(list2))
+        listn.insert(i-1,listm[i])
+print(''.join(listn))
