31c9a3a7ba8a3b9e46a7363e4dca9dd6bfb00ad8
Mohan <48092723+Mohan5545@users.noreply.github.com>
2019-07-05 09:40:22 +0530

    Create 7.py

---
+++ 7.py
@@ -0,0 +1,9 @@
+n=str(input())
+list1=list(n)
+list2=[]
+for i in range(0,len(list1)):
+    if(i%2==0):
+        list2.insert(i+1,list1[i])
+    elif(i%2==1):
+        list2.insert(i-1,list1[i])
+print(''.join(list2))
