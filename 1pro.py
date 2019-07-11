46938a8086cb8f6c7a4cd0570c7d0911a25d982b
Mohan <48092723+Mohan5545@users.noreply.github.com>
2019-07-11 15:08:35 +0530

    Update 1pro.py

--- 1pro.py
+++ 1pro.py
@@ -1,11 +1,11 @@
 def longest(m,n):
-        if(m in n):
-            return m
-        else:
-            return longest(st1[0:len(m)-1],n)
-j = int(input())
-a= []
-for _ in range(0,j):
-    a.append(input())
-a.sort()
-print(longest(a[0],a[j-1]))
+  if(m in n):
+     return m
+  else:
+     return longest(m[0:len(m)-1],n)
+num=int(input())
+char=[]
+for _ in range(0,num):
+   char.append(input())
+char.sort()
+print(longest(char[0],char[num-1]))
